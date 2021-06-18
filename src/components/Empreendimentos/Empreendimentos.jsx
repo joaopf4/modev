efd4d362add38a66f6aed113ba88a25a32c83921
Joao Pedro <joaopfa@hotmail.com>
2021-06-18 17:47:40 -0300

    finalizando sessao de empreendimentos

--- src/components/Empreendimentos/Empreendimentos.jsx
+++ src/components/Empreendimentos/Empreendimentos.jsx
@@ -3,24 +3,29 @@ import Imperatriz from "../../assets/empreendimentos/imperatriz_renault.jpg";
 import Luxemburgo from "../../assets/empreendimentos/luxemburgo_renault.jpg";
 import Monterey from "../../assets/empreendimentos/monterey_vertaz.jpg";
 import SantaLuzia from "../../assets/empreendimentos/sta_luzia_renault.jpg";
+import IconeMais from "../../assets/icones/icone-mais-2.png";
 
 function Empreendimentos() {
   return (
     <EmpreendimentosSection>
       <Grid>
         <Card bgImg={Imperatriz}>
+          <img alt="Ver mais" src={IconeMais} />
           <p>Imperatriz</p>
           <p>BELO HORIZONTE-MG</p>
         </Card>
         <Card bgImg={Luxemburgo}>
+          <img alt="Ver mais" src={IconeMais} />
           <p>Luxemburgo</p>
           <p>BELO HORIZONTE-MG</p>
         </Card> 
         <Card bgImg={Monterey}>
+          <img alt="Ver mais" src={IconeMais} />
           <p>Monterey</p>
           <p>BELO HORIZONTE-MG</p>
         </Card> 
         <Card bgImg={SantaLuzia}>
+          <img alt="Ver mais" src={IconeMais} />
           <p>Santa Luzia</p>
           <p>BELO HORIZONTE-MG</p>
         </Card> 
